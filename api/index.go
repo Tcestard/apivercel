58266efa99f3aa165530270ffbd2a40807abb1ba
Suzyo Nyirenda <suzyoeliasnyirenda@gmail.com>
2023-05-24 13:19:42 +0200

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,30 +1,32 @@
 package handler
 
 import (
+	"embed"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
 )
 
+//go:embed apivercel
+var embeddedExecutable embed.FS
+
 func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World from Golang serverless</h1>")
+
 	// Retrieve the embedded executable
-	executablePath := "myExecutable/apivercel"
-	executableBytes, err := ioutil.ReadFile(executablePath)
+	executablePath := "apivercel"
+	executableBytes, err := embeddedExecutable.ReadFile(executablePath)
 	if err != nil {
-		log.Println("Error reading embedded executable:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error reading embedded executable: %v", err)
 		return
 	}
 
 	// Create a temporary file to write the embedded executable
 	tmpFile, err := ioutil.TempFile("", "apivercel")
 	if err != nil {
-		log.Println("Error creating temporary file:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error creating temporary file: %v", err)
 		return
 	}
 	defer os.Remove(tmpFile.Name())
@@ -32,76 +34,32 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// Write the embedded executable to the temporary file
 	_, err = tmpFile.Write(executableBytes)
 	if err != nil {
-		log.Println("Error writing embedded executable to temporary file:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error writing embedded executable to temporary file: %v", err)
 		return
 	}
 
 	// Close the temporary file before copying it
 	tmpFile.Close()
 
-	// Create a new file outside the temporary directory
-	executableFile := "/tmp/apivercel" // Change the file path as needed
-	err = copyFile(tmpFile.Name(), executableFile)
+	// Make the temporary file executable
+	err = os.Chmod(tmpFile.Name(), 0755)
 	if err != nil {
-		log.Println("Error copying temporary file:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error making temporary file executable: %v", err)
 		return
 	}
 
-	// Make the new file executable
-	err = os.Chmod(executableFile, 0755)
-	if err != nil {
-		log.Println("Error making file executable:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Execute the file
-	cmd := exec.Command(executableFile)
+	// Execute the temporary file
+	cmd := exec.Command(tmpFile.Name())
 	err = cmd.Start()
 	if err != nil {
-		log.Println("Error executing the embedded executable:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error executing the embedded executable: %v", err)
 		return
 	}
 
 	// Wait for the command to finish
 	err = cmd.Wait()
 	if err != nil {
-		log.Println("Command execution failed:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Fprintf(w, "Command execution failed: %v", err)
 		return
 	}
-
-	// Build the response with the processing message
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Processing...\n")
-}
-
-// Copy the file from src to dst
-func copyFile(src, dst string) error {
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-
-	dstFile, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer dstFile.Close()
-
-	_, err = io.Copy(dstFile, srcFile)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
